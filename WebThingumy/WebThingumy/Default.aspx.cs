5b653f1b245f95fd0947bf3ade57aa0cb791e3d0
fzh2p0 <phillipssolutions@me.com>
2018-09-02 18:35:00 +0100

    clean up

--- WebThingumy/WebThingumy/Default.aspx.cs
+++ WebThingumy/WebThingumy/Default.aspx.cs
@@ -9,28 +9,8 @@ namespace WebThingumy
 {
     public partial class _Default : Page
     {
-        private int left = 10;
-        private int top = 10;
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-            }
-            else
-            {
-                //Also, I would add other checking to make sure that this is posted back by our script
-                string ControlID = string.Empty;
-                if (!String.IsNullOrEmpty(Request.Form["__EVENTTARGET"]))
-                {
-                    ControlID = Request.Form["__EVENTTARGET"];
-                }
-                //if (ControlID == HiddenField.ClientID)
-                //{
-                //    //On postback do our operation
-                //    string myVal = HiddenField.Value;
-                //    //etc...
-                //}
-            }
         }
     }
 }
